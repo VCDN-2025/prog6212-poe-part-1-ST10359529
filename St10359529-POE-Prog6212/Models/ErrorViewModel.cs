02318b1a5bab31f4c182aa68cfdac64d9d45c75d
lab_services_student <lab_services_student@lab7L95SR>
2025-09-16 15:47:12 +0200

    Add project files.

---
+++ St10359529-POE-Prog6212/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace St10359529_POE_Prog6212.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

ee074ea1888b38524014bce9ed4cd92c677dafaf
lab_services_student <lab_services_student@lab7L95SR>
2025-11-17 12:54:27 +0200

    added a folder and class with code

---
+++ St10359529-POE-Prog6212/Services/ClaimValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using St10359529_POE_Prog6212.Models;
+
+namespace St10359529_POE_Prog6212.Services
+{
+    public class ClaimValidator : AbstractValidator<Claim>
+    {
+        public ClaimValidator()
+        {
+            RuleFor(c => c.HoursWorked).GreaterThan(0).WithMessage("Hours must be > 0");
+            RuleFor(c => c.HourlyRate).InclusiveBetween(50, 500).WithMessage("Rate must be between 50-500");
+            RuleFor(c => c.TotalAmount).GreaterThan(0).WithMessage("Total must be > 0");
+        }
+    }
+}
